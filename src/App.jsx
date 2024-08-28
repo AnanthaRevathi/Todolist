051da12dae56fe309c264ce4d7c7897d338b6c9c
GROUP4PROJECTEXCELR <groupprojectexcelr@gmail.com>
2024-08-28 18:51:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './components/Todo'
+
+export const App = () => {
+  return (
+    <div className='bg-slate-600 grid py-4 min-h-screen'>
+      <Todo/>
+    </div>
+  )
+}
+export default App
